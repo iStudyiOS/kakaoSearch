2edbe297ec6e346dafef72fa0928cd4ae32f9c94
hello-SiOO <fltcy2039@naver.com>
2021-03-16 21:21:54 +0900

    create -수정

---
+++ APIDemo/APIDemo/ImgCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImgCell.swift
+//  APIDemo
+//
+//  Created by 임수정 on 2021/03/16.
+//
+
+import UIKit
+
+class ImgCell: UICollectionViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+}
